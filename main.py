78434456347fb07a44f6a66f39933ef88691effd
smalldirtybird <smalldirtybird@gmail.com>
2022-07-05 14:38:05 +0300

    Add several stars animation

--- main.py
+++ main.py
@@ -3,26 +3,6 @@ import time
 import curses
 
 
-def draw(canvas):
-    row, column = (5, 20)
-    curses.curs_set(False)
-    canvas.border()
-    while True:
-        canvas.refresh()
-        star_blink = blink(canvas, row, column)
-        star_blink.send(None)
-        time.sleep(1)
-        canvas.refresh()
-        star_blink.send(None)
-        time.sleep(1)
-        canvas.refresh()
-        star_blink.send(None)
-        time.sleep(1)
-        canvas.refresh()
-        star_blink.send(None)
-        time.sleep(1)
-
-
 async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
@@ -38,6 +18,31 @@ async def blink(canvas, row, column, symbol='*'):
         await asyncio.sleep(0)
 
 
-if __name__ == '__main__':
+def draw(canvas):
+    row = 5
+    animation_steps_number = 4
+    stars_count = 5
+    coroutines = []
+    zero_column_intent = 20
+    column_step = 10
+    for number in range(stars_count):
+        column = zero_column_intent + column_step * number
+        coroutines.append(blink(canvas, row, column))
+    curses.curs_set(False)
+    canvas.border()
+    while True:
+        canvas.refresh()
+        for num in range(animation_steps_number + 1):
+            for coroutine in coroutines.copy():
+                coroutine.send(None)
+                canvas.refresh()
+            time.sleep(1)
+
+
+def main():
     curses.update_lines_cols()
     curses.wrapper(draw)
+
+
+if __name__ == '__main__':
+    main()
