530569c582c2e871a18490ba6e5e4382dee82019
smalldirtybird <smalldirtybird@gmail.com>
2022-07-05 15:49:13 +0300

    Add starry sky generator

--- main.py
+++ main.py
@@ -1,45 +1,57 @@
 import asyncio
 import time
 import curses
+import random
 
-TIC_TIMEOUT = 1
+TIC_TIMEOUT = 0.1
 
 
 async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        await asyncio.sleep(0)
+        for _ in range(20):
+            await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0)
+        for _ in range(3):
+            await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        await asyncio.sleep(0)
+        for _ in range(5):
+            await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0)
+        for _ in range(3):
+            await asyncio.sleep(0)
+
+
+def generate_starry_sky(canvas):
+    max_rows, max_columns = curses.window.getmaxyx(canvas)
+    stars_count = max_columns
+    coords_seen = []
+    stars = []
+    for number in range(stars_count):
+        row = random.randint(1, max_rows - 1)
+        column = random.randint(1, max_columns - 1)
+        if [row, column] not in coords_seen:
+            stars.append(blink(canvas, row, column, symbol=random.choice('+*.:')))
+            coords_seen.append([row, column])
+    return stars
 
 
 def draw(canvas):
-    row = 5
-    stars_count = 5
-    zero_column_intent = 20
-    column_step = 10
-    coroutines = []
-    for number in range(stars_count):
-        column = zero_column_intent + column_step * number
-        coroutines.append(blink(canvas, row, column))
+    stars = generate_starry_sky(canvas)
     curses.curs_set(False)
     canvas.border()
     while True:
         try:
-            for coroutine in coroutines.copy():
-                coroutine.send(None)
+            for star in stars.copy():
+                star.send(None)
                 canvas.refresh()
             time.sleep(TIC_TIMEOUT)
         except StopIteration:
             break
-            
+
 
 def main():
     curses.update_lines_cols()
