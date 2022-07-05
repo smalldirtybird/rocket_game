9ebaefb02950e5cdfc8ac89c0e11f48aa203ec64
smalldirtybird <smalldirtybird@gmail.com>
2022-07-05 16:29:14 +0300

    Add gun shot animation

--- main.py
+++ main.py
@@ -7,48 +7,72 @@ TIC_TIMEOUT = 0.1
 
 
 async def blink(canvas, row, column, symbol='*'):
+    time_delay = random.randint(0, 25)
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        for _ in range(20):
+        for _ in range(time_delay + 20):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        for _ in range(3):
+        for _ in range(time_delay + 3):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        for _ in range(5):
+        for _ in range(time_delay + 5):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        for _ in range(3):
+        for _ in range(time_delay + 3):
             await asyncio.sleep(0)
 
 
-def generate_starry_sky(canvas):
-    max_rows, max_columns = curses.window.getmaxyx(canvas)
+def generate_starry_sky(canvas, max_rows, max_columns):
     stars_count = max_columns
     coords_seen = []
     stars = []
     for number in range(stars_count):
-        row = random.randint(1, max_rows - 1)
-        column = random.randint(1, max_columns - 1)
+        row = random.randint(1, max_rows)
+        column = random.randint(1, max_columns)
         if [row, column] not in coords_seen:
             stars.append(blink(canvas, row, column, symbol=random.choice('+*.:')))
             coords_seen.append([row, column])
     return stars
 
 
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    row, column = start_row, start_column
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+    row += rows_speed
+    column += columns_speed
+    symbol = '-' if columns_speed else '|'
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+    curses.beep()
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
 def draw(canvas):
-    stars = generate_starry_sky(canvas)
+    window_height, window_width = curses.window.getmaxyx(canvas)
+    stars = generate_starry_sky(canvas, window_height - 1, window_width - 1)
     curses.curs_set(False)
     canvas.border()
+    gun_shot = fire(canvas, window_height - 1, (window_width - 1) / 2)
     while True:
         try:
             for star in stars.copy():
                 star.send(None)
                 canvas.refresh()
             time.sleep(TIC_TIMEOUT)
+            gun_shot.send(None)
         except StopIteration:
             break
 
