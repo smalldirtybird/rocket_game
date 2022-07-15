f498ef4a6165b825cbda28140682955e5991c8f8
smalldirtybird <smalldirtybird@gmail.com>
2022-07-15 17:18:30 +0300

    Update try/except block

--- main.py
+++ main.py
@@ -136,10 +136,11 @@ def draw(canvas, animation_frames):
         try:
             for coroutine in coroutines.copy():
                 coroutine.send(None)
-            canvas.refresh()
-            time.sleep(TIC_TIMEOUT)
         except StopIteration:
             coroutines.remove(coroutine)
+        finally:
+            canvas.refresh()
+            time.sleep(TIC_TIMEOUT)
 
 
 def main():
